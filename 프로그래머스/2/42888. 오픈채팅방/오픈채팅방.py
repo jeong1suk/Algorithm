93e2bd34e21b7d6774e5ef6a2df64a4373864464
WonsukJeong <77705600+jeong1suk@users.noreply.github.com>
2024-05-27 22:27:57 +0900

    [level 2] Title: 오픈채팅방, Time: 152.84 ms, Memory: 47.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,24 @@
+def solution(record):
+    answer = []
+    user_id = dict()
+    rec = []
+    for r in record:
+        op, ID = r.split()[0], r.split()[1]
+        if op == 'Enter' or op == 'Change':
+            name = r.split()[2]
+            user_id[ID] = name
+            # if ID in user_id.keys():
+                # user_id[ID] = name
+        # elif op == 'Change':
+        #     name = r.split()[2]
+        #     if ID in user_id.keys():
+        #         user_id[ID] = name
+        rec.append([op, ID])
+    
+    for o, i in rec:
+        if o == 'Enter':
+            answer.append(f"{user_id[i]}님이 들어왔습니다.")
+        elif o == 'Leave':
+            answer.append(f"{user_id[i]}님이 나갔습니다.")
+
+    return answer
