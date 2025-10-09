df919611549ad02af45444b26204e57c6a71430d
WonsukJeong <77705600+jeong1suk@users.noreply.github.com>
2025-10-09 10:44:43 +0900

    [level 3] Title: 베스트앨범, Time: 0.08 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,29 +1,22 @@
 def solution(genres, plays):
     answer = []
-    dic = {}
-    '''
-    장르별 베스트 2개씩
-    제일 많이 재생 장르 먼저 수록, 장르 내에서 많이 재생된 노래 수록, 고유 번호 낮은 노래
-    classic = [500, 150, 800]
-    pop = [600, 2500]
-    이러면 pop 먼저고 2500 600,  classic 800 500
-    장르는 100개 미만, 속한 곡이 하나일 수도 있음 play는 고유함
-    '''
+    d1 = {}
+    d2 = {}
+    
     for i, (genre, play) in enumerate(zip(genres, plays)):
-        # print(i, genre, play)
-        if genre not in dic:
-            dic[genre] = {'t': 0, 'l': []}
-        dic[genre]['l'].append({'v':play, 'i':i})
-        dic[genre]['t'] += play
-    
-    dic = list(dic.values())
-    # print("분류 전: ", dic)
-    dic = sorted(dic, key=lambda x:x['t'], reverse=True)
-    # print("sort: ", dic)
-    
-    for i in range(len(dic)):
-        v = dic[i]['l']
-        v = sorted(v, key=lambda x:x['v'], reverse=True)
-        for j in range(2 if len(v) >= 2 else len(v)):
-            answer.append(v[j]['i'])
+        if genre not in d1:
+            d1[genre] = [(i, play)]
+        else:
+            d1[genre].append((i, play))
+        
+        if genre not in d2:
+            d2[genre] = play
+        else:
+            d2[genre] += play
+    # print(d1)
+    # print(d2)
+    for (k, v) in sorted(d2.items(), key=lambda x:x[1], reverse=True):
+        # print(k, v)
+        for (i, p) in sorted(d1[k], key=lambda x:x[1], reverse=True)[:2]:
+            answer.append(i)
     return answer
